17d7316c006374e6ab6648f912eb615877c07b75
lorenzocasc <lorenzo.cascone17@gmail.com>
2023-08-27 15:48:20 +0200

    update script.js
    Possibility to play again

--- project/script.js
+++ project/script.js
@@ -44,6 +44,15 @@ document.querySelector('.check').addEventListener('click', function() {
         }
     }
 
-    
+    document.querySelector('.again').addEventListener('click', function() {
+        score = 20;
+        secretNumber = Math.trunc(Math.random() * 20) +1;
+        displayMessage('Start guessing...');
+        labelScore(score);
+        document.querySelector('.number').textContent = '?';
+        document.querySelector('.guess').value = '';
+        document.querySelector('body').style.backgroundColor = '#222';
+        document.querySelector('.number').style.width = '15rem';
+    });
 
 });
